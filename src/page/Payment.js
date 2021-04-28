129f2b71565b422efc54b660d46a6e4d53741171
Solbon13 <Solbon13@yandex.ru>
2021-04-28 15:48:52 +0800

    deploy clarification on commission

--- src/page/Payment.js
+++ src/page/Payment.js
@@ -5,6 +5,7 @@ import PaymentTable from '../components/PaymentTable/PaymentTable'
 const Payment = () => {
     return (
         <div>
+            Цена на Teqra фиксирована + для того чтоб положить на кошелек цена доллара выше примерно на 5 рублей
             <PaymentForm/>
             <PaymentTable/>
         </div>
